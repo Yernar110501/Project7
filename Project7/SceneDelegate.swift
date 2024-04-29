504c999080fe0cc7b670ba3bcc27bf91ee2d69a4
yernar <yernar.baiginzheyev@gmail.com>
2024-04-29 20:59:02 +0500

    Initial Commit

---
+++ Project7/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Project7
+//
+//  Created by Yernar Baiginzheyev on 29.04.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ Project7/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Yernar Baiginzheyev on 29.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
